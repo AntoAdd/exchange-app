ebce1eebf25f44cf9fa129d2f50db919e8d83e74
AntoAdd <antonio.addesa93@gmail.com>
2024-04-16 19:39:33 +0200

    backend base spring boot project added

---
+++ exchange-app-backend/src/main/java/it/unicam/cs/pawm/exchangeappbackend/ExchangeAppApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.pawm.exchangeappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeAppApplication.class, args);
+	}
+
+}
---
+++ exchange-app-backend/src/test/java/it/unicam/cs/pawm/exchangeappbackend/ExchangeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.pawm.exchangeappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
