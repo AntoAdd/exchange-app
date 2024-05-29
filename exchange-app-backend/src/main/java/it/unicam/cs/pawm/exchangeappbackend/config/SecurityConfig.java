6efd2ded265f9039cab236dbe8034bc76e017ac0
AntoAdd <antonio.addesa93@gmail.com>
2024-05-29 14:20:23 +0200

    Security configuration modified to work with jwt tokens

--- exchange-app-backend/src/main/java/it/unicam/cs/pawm/exchangeappbackend/config/SecurityConfig.java
+++ exchange-app-backend/src/main/java/it/unicam/cs/pawm/exchangeappbackend/config/SecurityConfig.java
@@ -1,34 +1,108 @@
 package it.unicam.cs.pawm.exchangeappbackend.config;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private RSAKey rsaKey;
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService){
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        return new ProviderManager(authProvider);
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
             .csrf(AbstractHttpConfigurer::disable)
-            .httpBasic(Customizer.withDefaults())
             .authorizeHttpRequests(auth -> {
-                auth.requestMatchers("/register").permitAll();
+                auth.requestMatchers("/register", "/authenticate").permitAll();
                 auth.anyRequest().authenticated();
             })
-            .formLogin(Customizer.withDefaults())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
             .build();
     }
 
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() throws NoSuchAlgorithmException {
+        KeyPairGenerator kg = KeyPairGenerator.getInstance("RSA");
+        kg.initialize(2048);
+        KeyPair kp = kg.generateKeyPair();
+
+        RSAPublicKey publicKey = (RSAPublicKey) kp.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) kp.getPrivate();
+
+        rsaKey = new RSAKey.Builder(publicKey)
+            .privateKey(privateKey)
+            .keyID(UUID.randomUUID().toString())
+            .build();
+
+        JWKSet set = new JWKSet(rsaKey);
+
+        return new ImmutableJWKSet<>(set);
+    }
+    @Bean
+    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwks){
+        return new NimbusJwtEncoder(jwks);
+    }
+    @Bean
+    public JwtDecoder jwtDecoder() throws JOSEException {
+        return NimbusJwtDecoder.withPublicKey(rsaKey.toRSAPublicKey()).build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- exchange-app-backend/src/main/java/it/unicam/cs/pawm/exchangeappbackend/controllers/AuthorizationController.java
+++ exchange-app-backend/src/main/java/it/unicam/cs/pawm/exchangeappbackend/controllers/AuthorizationController.java
@@ -5,32 +5,40 @@ import it.unicam.cs.pawm.exchangeappbackend.entities.Role;
 import it.unicam.cs.pawm.exchangeappbackend.mappers.UserMapper;
 import it.unicam.cs.pawm.exchangeappbackend.repositories.RoleRepository;
 import it.unicam.cs.pawm.exchangeappbackend.services.UserRegistrationService;
-import org.springframework.http.MediaType;
+import it.unicam.cs.pawm.exchangeappbackend.services.security.JwtTokenService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
 import java.util.Set;
-@CrossOrigin(origins = "http://localhost:3000")
+
 @RestController
-public class RegistrationController {
+public class AuthorizationController {
     private final UserRegistrationService userRegistrationService;
     private final PasswordEncoder passwordEncoder;
     private final UserMapper userMapper;
     private final RoleRepository roleRepository;
+    private final JwtTokenService tokenService;
+    private final AuthenticationManager authManager;
 
-    public RegistrationController(UserRegistrationService userRegistrationService,
-                                  PasswordEncoder passwordEncoder,
-                                  UserMapper userMapper,
-                                  RoleRepository roleRepository) {
+    public AuthorizationController(UserRegistrationService userRegistrationService,
+                                   PasswordEncoder passwordEncoder,
+                                   UserMapper userMapper,
+                                   RoleRepository roleRepository,
+                                   JwtTokenService tokenService,
+                                   AuthenticationManager authManager) {
         this.userRegistrationService = userRegistrationService;
         this.passwordEncoder = passwordEncoder;
         this.userMapper = userMapper;
         this.roleRepository = roleRepository;
+        this.tokenService = tokenService;
+        this.authManager = authManager;
     }
+
     @PostMapping(value = "/register")
     public void registerUser(@RequestParam(name = "firstName") String firstName,
                              @RequestParam(name = "lastName") String lastName,
@@ -40,11 +48,20 @@ public class RegistrationController {
         var userCreationDto = new UserCreationDto(firstName, lastName, username, password, address);
         var user = userMapper.toUserCreationEntity(userCreationDto);
         if (!userRegistrationService.checkIfAlreadyRegistered(user)){
-            String encodedPassword = passwordEncoder.encode(user.getPassword());
+            String encodedPassword = "{bcrypt}" + passwordEncoder.encode(user.getPassword());
             Role role = roleRepository.findById(1L).orElseGet(() -> new Role("user"));
             user.setPassword(encodedPassword);
             user.setRoles(Set.of(role));
             userRegistrationService.createUser(user);
         }
+    }
+
+    @PostMapping("/authenticate")
+    public String login(@RequestParam(name = "username") String username,
+                        @RequestParam(name = "password") String password) {
+        Authentication auth = authManager.authenticate(
+            new UsernamePasswordAuthenticationToken(username, password)
+        );
+        return tokenService.generateJwtToken(auth);
     }
 }
