7344a04294cfc1474d442f6cab870e3120370836
AntoAdd <antonio.addesa93@gmail.com>
2025-05-12 19:31:47 +0200

    Notification controller interface modified

--- exchange-app-backend/src/main/java/it/unicam/cs/pawm/exchangeappbackend/controllers/NotificationsController.java
+++ exchange-app-backend/src/main/java/it/unicam/cs/pawm/exchangeappbackend/controllers/NotificationsController.java
@@ -4,9 +4,7 @@ import it.unicam.cs.pawm.exchangeappbackend.dto.NotificationDTO;
 import it.unicam.cs.pawm.exchangeappbackend.mappers.NotificationMapper;
 import it.unicam.cs.pawm.exchangeappbackend.services.AuthService;
 import it.unicam.cs.pawm.exchangeappbackend.services.NotificationService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.List;
@@ -24,8 +22,8 @@ public class NotificationsController {
         this.authService = authService;
     }
 
-    @GetMapping("/subscribe")
-    public SseEmitter subscribe(String username) {
+    @GetMapping("/subscribe/{username}")
+    public SseEmitter subscribe(@PathVariable String username) {
             SseEmitter emitter = new SseEmitter();
 
             emitter.onCompletion(() -> notificationService.unsubscribe(username, emitter));
